842755908b52a0ab3b065831d3b24fde13f13562
RS <radhi@pop-os.localdomain>
2021-04-03 13:54:22 +0100

    Split the code into a header file (.h) and a .cpp file for better expandability, also added some viz functions

---
+++ Fuzzy_Logic/Fuzzy_system.h
@@ -0,0 +1,17 @@
+#ifndef __FUZZY_SYS_H__
+  #define __FUZZY_SYS_H__
+  #include <Fuzzy.h>
+
+  typedef struct{
+    float fire_low;
+    float fire_med;
+    float fire_high;
+  }fire_conf;
+  
+  void setup_fuzzy(); // Sets up the fuzzy system's inputs, output, and rule-base
+  void set_fuzzy_inputs(float input_t, float input_s, float input_dt, float input_ds);
+  void fuzzify_system();
+  float defuzzify_system(); // Get system's output
+  fire_conf get_fire_conf(); // Get fire confidences from the fuzzy system
+
+#endif
